b21a02ee3f2dca0f5f4b9c4d86ff11dce1c30896
angelvenchev <angelvenchev@yahoo.com>
2014-02-16 13:50:12 +0200

    Refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Router.init do
-  match method: :get,  path: '/auth/register',
+  match method: :get, path: '/auth/register',
   controller: 'AuthenticationController', action: 'register_new'
 
-  match method: :post, path: '/auth/register',
+  match method: :post,path: '/auth/register',
   controller: 'AuthenticationController', action: 'register'
 
-  match method: :get,  path: '/auth/login',
+  match method: :get, path: '/auth/login',
   controller: 'AuthenticationController', action: 'show_login'
 
-  match method: :post, path: '/auth/login',
+  match method: :post,path: '/auth/login',
   controller: 'AuthenticationController', action: 'login'
 
-  match method: :get,  path: '/',
+  match method: :get, path: '/',
   controller: 'HomeController', action: 'index'
+
+  match method: :get, path: '/auth/logout',
+  controller: 'AuthenticationController', action: 'logout'
 
 end
