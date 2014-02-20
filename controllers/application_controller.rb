0c3ca64df7b7f5e7b8e35d4ef5447a62af58862d
angelvenchev <angelvenchev@yahoo.com>
2014-02-20 15:06:32 +0200

    Add helper methods for controllers

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -11,4 +11,17 @@ class ApplicationController < Delegator
   def __setobj__(obj)
     @app = obj
   end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find session[:user_id] if logged_in?
+  end
+
+  def redirect_if_not_logged_in
+    redirect '/' unless logged_in?
+  end
+
 end
