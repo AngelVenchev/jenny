4181ef19152739d7cc4bd31f5d25d12ab51ef5a6
angelvenchev <angelvenchev@yahoo.com>
2014-02-21 13:20:21 +0200

    Remove useless comments

--- controllers/iteration_controller.rb
+++ controllers/iteration_controller.rb
@@ -45,31 +45,4 @@ END
       redirect "/projects/#{params[:project_id]}/iterations/new"
     end
   end
-
-  # def index(params)
-  #   redirect_if_not_logged_in
-  #   user = User.find(session[:user_id])
-  #   locals = {user: user, home: '/projects'}
-  #   haml :'project/index', locals: locals
-  # end
-
-  # def edit(params)
-  #   redirect_if_not_logged_in
-  #   haml :'project/edit', locals: common_locals(params[:id])
-  # end
-
-  # def show(params)
-  #   redirect_if_not_logged_in
-
-  #   locals = common_locals(params[:id])
-
-  #   all_iterations = Iteration.all.order("start_date ASC")
-
-  #   session[:offset] = 0 if params[:current] or not session[:offset]
-  #   session[:offset] += params[:step].to_i if session[:offset].abs < all_iterations.size
-
-  #   locals[:iterations] = current_iterations(all_iterations)
-
-  #   haml :'project/show', locals: locals
-  # end
 end
