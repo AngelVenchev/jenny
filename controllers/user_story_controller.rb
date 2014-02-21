4b28a2847f36a07cf35bf332bdcc8bf3aae4da0f
angelvenchev <angelvenchev@yahoo.com>
2014-02-21 19:50:37 +0200

    Add UserStory creation action

--- controllers/user_story_controller.rb
+++ controllers/user_story_controller.rb
@@ -3,7 +3,6 @@ class UserStoryController < ApplicationController
 
   def new(params)
     redirect_if_not_logged_in
-    params.inspect
     locals =
     {
       user: current_user,
@@ -16,6 +15,35 @@ class UserStoryController < ApplicationController
   end
 
   def create(params)
-    params.inspect
+    redirect_if_not_logged_in
+    story = UserStory.new(
+      title: params[:title],
+      description: params[:description],
+      status: params[:status].to_i,
+      ready: !!params[:ready],
+      blocked: !!params[:blocked],
+      blocked_reason: params[:blocked_reason],
+      task_estimate: params[:task_estimate].to_f,
+      actual: params[:actual].to_f,
+      to_do: params[:to_do].to_f
+    )
+
+    locals =
+    {
+      user: current_user,
+      project_id: params[:project_id],
+      home: "/projects/#{params[:project_id]}"
+    }
+    if story.save
+      Project.find(params[:project_id]).user_stories << story
+      if params[:iteration_id].to_i != -1
+        Iteration.find(params[:iteration_id].to_i).user_stories << story
+      end
+      flash[:notice] = "Successfully created user story #{story.title}"
+      redirect "/projects/#{params[:project_id]}"
+    else
+      flash[:error] = "Unable to create user story #{story.title}"
+      redirect "/projects/#{params[:project_id]}/user_stories/new"
+    end
   end
 end
